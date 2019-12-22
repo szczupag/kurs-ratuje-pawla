64ea6e43a749b661e9f77caf89eb4ca812757686
szczupag <jadziutka@gmail.com>
2019-12-22 23:19:01 +0100

    change start page layout

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import Button from '../Button'
+import close from '../assets/close.svg'
+import min from '../assets/minimize.svg'
+import max from '../assets/maximize.svg'
+import pointer from '../assets/pointer.svg'
 
 import './style.css'
 
 const Main = () => (
-    <div className="main-container">
-        <h1>Witaj na kursie z podstaw HTML i CSS!</h1>
-        <div className="btn-wrapper-center">
-            <Button
-                to="/wprowadzenie"
-            >
-                Zaczynamy
-            </Button>
+    <div className="main-container fb">
+        <div className="fb-banner">
+            <div className="fb-header">
+                <p className="fb-title">Ratujemy Pawła</p>
+                <p><img src={min} /></p>
+                <p><img src={max} /></p>
+                <p><img src={close} /></p>
+            </div>
+            <div className="fb-content">
+                <p>Kurs</p>
+                <p style={{ fontSize: '2.8rem' }}>Podstawy HTML i CSS</p>
+            </div>
+            <div className="fb-footer">
+                <Button
+                    to="/wprowadzenie"
+                >
+                    Zaczynamy
+                </Button>
+            </div>
+        </div>
+        <div className="fb-link">
+            <a target="_blank" href="siepomaga.pl/ratujemy-pawla">siepomaga.pl/ratujemy-pawla</a>
         </div>
     </div>
 )
