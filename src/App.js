20fc85282366e7593f1b9e56acfadd8b123c0507
Agata Szczuka <agata.szczuka@bitgamelabs.com>
2019-12-24 13:34:35 +0100

    add scroll to top component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
-    <Router history={history}>
+    <Router
+      history={history}
+    >
+      <ScrollToTop />
       <Routes />
     </Router>
   );
