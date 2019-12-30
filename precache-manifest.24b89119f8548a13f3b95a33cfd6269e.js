f2b8aee9be0db4c4415ada25bd7b09d662b0bed7
Agata Szczuka <agata.szczuka@bitgamelabs.com>
2019-12-30 09:41:06 +0100

    Updates

---
+++ precache-manifest.24b89119f8548a13f3b95a33cfd6269e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc3ce356f4fe7ca20acef8c3d926db2",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/index.html"
+  },
+  {
+    "revision": "15977522c572fc851f65",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/css/main.d48deeb3.chunk.css"
+  },
+  {
+    "revision": "3b15dbb56032ba97dfa1",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/js/2.57fa41d1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/js/2.57fa41d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15977522c572fc851f65",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/js/main.466fc4db.chunk.js"
+  },
+  {
+    "revision": "16597d8fc944a25b680c",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/js/runtime-main.e65e7a00.js"
+  },
+  {
+    "revision": "398a8253e4db8c5234ba8800643e220f",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/media/background.398a8253.png"
+  },
+  {
+    "revision": "a23382919756e3ae3b4229566363269a",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/media/close.a2338291.svg"
+  },
+  {
+    "revision": "cae758fbf49ab60410e0fb263d660946",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/media/e00_diagram.cae758fb.png"
+  },
+  {
+    "revision": "3a51e934a41f0f8c65d45e3c0b7113b4",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/media/e01_struktura.3a51e934.png"
+  },
+  {
+    "revision": "2cd583521ffb094db29dce8e36277531",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/media/maximize.2cd58352.svg"
+  },
+  {
+    "revision": "7a132f753f036bda438b22c5f26f2331",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/media/minimize.7a132f75.svg"
+  },
+  {
+    "revision": "29a7e036d3af6ed8ffe4d9761650d7a4",
+    "url": "https://szczupag.github.io/kurs-ratuje-pawla/static/media/pointer.29a7e036.svg"
+  }
+]);
